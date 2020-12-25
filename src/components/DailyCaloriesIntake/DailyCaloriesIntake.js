586eda3215a541de09aa295dbd9e77d250df2952
mikiteek <mikiteek@gmail.com>
2020-12-25 15:35:29 +0200

    Create DailyCaloriesIntake component

---
+++ src/components/DailyCaloriesIntake/DailyCaloriesIntake.js
@@ -0,0 +1,40 @@
+import React, {Component} from "react";
+import {connect} from "react-redux";
+import userSelector from "../../redux/user/userSelector";
+import styles from "./DailyCaloriesIntake.module.scss";
+
+class DailyCaloriesIntake extends Component {
+  render() {
+    const {summary: {notAllowedCategories, dayNormCalories}} = this.props;
+    const sectionStyles = [styles.dailySection, "container"].join(" ");
+    return (
+      <div className={styles.section}>
+        <section className={sectionStyles}>
+          <h2 className={styles.dailySectionTitle}>Ваша рекомендуемая суточная норма калорий составляет</h2>
+          <div className={styles.dailySectionBlock}>
+            {dayNormCalories &&
+            <p className={styles.caloriesValue}>{dayNormCalories} <span className={styles.caloriesValueSpan}>ккал</span></p>
+            }
+            <h3 className={styles.productsListTitle}>Продукты, которые вам не рекомендуется употреблять</h3>
+            {notAllowedCategories &&
+            <ul>
+              {notAllowedCategories.map((category, ind) => (
+                <li key={category} className={styles.productsItem}>{ind + 1}. <span className={styles.productsItemCategory}>{category}</span></li>
+              ))}
+            </ul>
+            }
+          </div>
+          <div className={styles.buttonSubmitBlock}>
+            <button className={styles.buttonSubmit} type="button">Начать худеть</button>
+          </div>
+        </section>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  summary: userSelector.getSummary(state),
+});
+
+export default connect(mapStateToProps)(DailyCaloriesIntake);
