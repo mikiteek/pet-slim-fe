3ec91fb7cd0963a4cf937e3d58842c04f28df8ab
mikiteek <mikiteek@gmail.com>
2020-12-29 13:52:14 +0200

    Refactor BurgerMenu

--- src/components/BurgerMenu/BurgerMenu.js
+++ src/components/BurgerMenu/BurgerMenu.js
@@ -1,12 +1,31 @@
 import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
 import styles from "./BurgerMenu.module.scss";
-import Navigation from "../Navigation";
 
 class BurgerMenu extends Component {
   render() {
     return (
       <div className={styles.burgerMenu}>
-        <Navigation/>
+        <ul className={styles.navList}>
+          <li className={styles.navListItem}>
+            <NavLink
+              to={"/diary"}
+              className={styles.navListItemLink}
+              activeClassName={styles.navListItemLinkActive}
+            >
+              Дневник
+            </NavLink>
+          </li>
+          <li className={styles.navListItem}>
+            <NavLink
+              to={"/calc"}
+              className={styles.navListItemLink}
+              activeClassName={styles.navListItemLinkActive}
+            >
+              Калькулятор
+            </NavLink>
+          </li>
+        </ul>
       </div>
     );
   }
