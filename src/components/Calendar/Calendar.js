3bef3a8ebd76807cc2b634ed5b77d07255ab6a2f
mikiteek <mikiteek@gmail.com>
2021-01-05 17:32:40 +0200

    Calendar styles

--- src/components/Calendar/Calendar.js
+++ src/components/Calendar/Calendar.js
@@ -1,38 +1,62 @@
 import React, {Component} from "react";
 import CalendarReact from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import {connect} from "react-redux";
+
+import diaryActions from "../../redux/diary/diaryActions";
+import diarySelector from "../../redux/diary/diarySelector";
+
 import {ReactComponent as CalendarIcon} from "../../static/images/calendar/calendar-icon.svg";
+import styles from "./Calendar.module.scss";
 
 class Calendar extends Component {
   state = {
     showCalendar: false,
-    value: new Date(Date.now()),
   }
 
   handleClickCalendarBtn = () => {
     this.setState(prevState => ({showCalendar: !prevState.showCalendar}))
-    console.log("state: ", this.state);
   }
 
   handleChangeCalendar = (input) => {
-    this.setState({value: input})
+    this.props.onSetCurrentDate(input);
+  }
+
+  toFormatTime = (time) => {
+    return `${time.getDate().toString().padStart(2, "0")}.${(time.getMonth() + 1).toString().padStart(2, "0")}.${time.getFullYear()}`
   }
 
   render() {
-    const {showCalendar, value} = this.state;
-    console.log("value", value)
+    const {showCalendar} = this.state;
+    const {date, className} = this.props;
     return (
-      <div>
-        <div onClick={this.handleClickCalendarBtn}>
-          <CalendarIcon/>
+      <div className={className? className: styles.calendarComponent}>
+        <div className={styles.dateBlock}>
+          <time className={styles.date} dateTime={date}>{this.toFormatTime(date)}</time>
+          <div onClick={this.handleClickCalendarBtn} className={styles.iconBtn}>
+            <CalendarIcon/>
+          </div>
         </div>
-        {showCalendar && <CalendarReact
-          onChange={this.handleChangeCalendar}
-          value={value}
-        />}
+        {showCalendar &&
+          <div className={styles.calendarBlock}>
+            <CalendarReact
+              onChange={this.handleChangeCalendar}
+              value={date}
+              className={styles.calendar}
+            />
+          </div>
+        }
       </div>
     );
   }
 }
 
-export default Calendar;
+const mapStateToProps = state => ({
+  date: diarySelector.getCurrentDate(state),
+});
+
+const mapDispatchToProps = {
+  onSetCurrentDate: diaryActions.setCurrentDate,
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Calendar);
